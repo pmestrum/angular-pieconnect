5dd95f2d9ff8ebcb86e7e0273be812d10d9ef209
Paul Mestrum <paul.mestrum@delaware.pro>
2018-07-18 23:17:16 +0200

    data normalization

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,14 +10,46 @@ export class DataService {
       console.log('in loadData');
       Tabletop.init({
         key: 'https://docs.google.com/spreadsheets/d/1N_P4SpddLB-14wCst-2uzfjfgDq2qfN4rWxRGohxGGY/edit?usp=sharing',
-        callback: function (data, tabletop) {
+        callback: (data, tabletop) => {
           console.log(data);
           console.log(tabletop);
-          resolve({data, tabletop});
+          const elements = this.normalizeData(data);
+          resolve({ data, tabletop, ...elements});
         },
         simpleSheet: false,
         parseNumbers: true
       })
     });
+  }
+
+  private normalizeData(data) {
+    const arrayToObjectReducer = (key) => (res, lang) => {
+      return {
+        ...res,
+        [lang[key]]: lang,
+      }
+    };
+    const langElements = data.LANG.elements.reduce(arrayToObjectReducer('LANG_ID'), {});
+    const lawElements = data.LAW.elements.reduce(arrayToObjectReducer('LAW_ID'), {});
+    const termElements = data.TERM.elements.reduce(arrayToObjectReducer('LANG_ID'), {});
+    Object.keys(termElements).forEach(termId => {
+      const term = termElements[termId];
+      term.lang = langElements[term.LANG_ID];
+      if (!term.lang.terms) {
+        term.lang.terms = [];
+      }
+      term.lang.terms.push(term);
+      term.laws = data.LAWAPP.elements.filter(link => link.TERM_ID === term.TERM_ID).map(link => {
+        const lawElement = lawElements[link.LAW_ID];
+        if (!lawElement.terms) {
+          lawElement.terms = [];
+        }
+        lawElement.terms.push(term);
+        return lawElements[link.LAW_ID];
+      })
+    });
+
+    console.log({langElements, termElements, lawElements});
+    return langElements;
   }
 }
